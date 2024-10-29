e8e66ebd38f7c8448097d1d609078902391ee208
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-29 13:50:49 +0300

    will refactor all send money logics bcz it's overhead the db

--- pkg/money/send_money.go
+++ pkg/money/send_money.go
@@ -6,6 +6,8 @@ import (
 	"slices"
 	"time"
 
+	// "time"
+
 	"github.com/RazanakotoMandresy/go-gin-banks/pkg/common/models"
 	"github.com/RazanakotoMandresy/go-gin-banks/pkg/middleware"
 	"github.com/gin-gonic/gin"
@@ -20,10 +22,8 @@ func (h handler) SendMoney(ctx *gin.Context) {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 		return
 	}
-
 	userToSend := ctx.Param("uuid")
 	body := new(sendMoneyRequest)
-
 	if err := ctx.BindJSON(&body); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 		return
@@ -38,7 +38,10 @@ func (h handler) SendMoney(ctx *gin.Context) {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 		return
 	}
-	// maka anle userRecepteur tokony par uuid na par AppUserName
+	if err := middleware.IsTruePassword(userConnected.Password, body.Passwords); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
 	userRecepteur, err := h.GetUserByuuid(userToSend)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
@@ -46,23 +49,21 @@ func (h handler) SendMoney(ctx *gin.Context) {
 	}
 	// check si l'envoyeur essayent d'envoyer plus d'argent que ce qu'il en a
 	if value > userConnected.Moneys {
-		err := fmt.Errorf("impossible d'envoyer votre argent %v l'argent que vous voulez envoyer est %v", userConnected.Moneys, value)
+		err := fmt.Errorf("it's impossible to send money of value %v , because money in your account %v", value, userConnected.Moneys)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 		return
 	}
 	// check si l'userConnecter est la meme que celui qui il essaye d'envoyer de l'argent
 	if uuidConnectedStr == userRecepteur.UUID {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": "vous ne pouvez pas envoyer de l'argent a vous meme "})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": " you cannot send money to your self "})
 		return
 	}
-	// 	message si tous se passe bien
-	// message := fmt.Sprintf("%v a envoye un argent d'un montant de %v a %v", userConnected.AppUserName, value, userRecepteur.AppUserName)
 	userConnected.Moneys = userConnected.Moneys - value
 	userRecepteur.Moneys = (userRecepteur.Moneys + value)
-	// save les money ao anaty user satria ireo tables fatsy uuid simplement ril
+
 	h.DB.Save(userRecepteur)
 	h.DB.Save(userConnected)
-	// la models ho creena
+
 	moneyTransaction, err := h.dbManipulationSendMoney(userConnected, userRecepteur, body)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
@@ -70,8 +71,6 @@ func (h handler) SendMoney(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, gin.H{
 		"moneyTransaction": &moneyTransaction,
-		"userSender":       &userConnected,
-		"userReceive":      &userToSend,
 	})
 }
 
@@ -81,26 +80,26 @@ func (h handler) dbManipulationSendMoney(userConnected, userRecepteur *models.Us
 	if found {
 		return nil, fmt.Errorf("user with uuid %s already blocked position : %v all blocekdAccount: %v", userRecepteur.UUID, n, userConnected.BlockedAcc)
 	}
-	res := h.DB.Where("send_by = ? AND sent_to = ?", userConnected.UUID, userRecepteur.UUID).First(&moneyTransaction)
-	resume := fmt.Sprintf("%v a envoyer la somme de %v a %v a l'instant%v ", userConnected.AppUserName, body.Value, userRecepteur.AppUserName, time.Now())
-	if res.Error != nil {
-		fmt.Printf("no transaction betwen %s and %s creating a new one...", userConnected.UUID, userRecepteur.UUID)
+	resume := fmt.Sprintf("%v just send %v to %v now :%v ", userConnected.AppUserName, body.Value, userRecepteur.AppUserName, time.Now().Format(time.RFC1123Z))
+	if res := h.DB.Where("send_by = ? AND sent_to = ?", userConnected.UUID, userRecepteur.UUID).First(&moneyTransaction); res.Error != nil {
+		fmt.Printf("no transaction betwen %s and %s creating a new one.....", userConnected.UUID, userRecepteur.UUID)
 		moneyTransaction.ID = uuid.New()
+		moneyTransaction.Resume = resume
+		moneyTransaction.Totals = body.Value
 		moneyTransaction.SendBy = userConnected.UUID
 		moneyTransaction.SentTo = userRecepteur.UUID
-		moneyTransaction.Totals = body.Value
-		moneyTransaction.Resume = resume
 		moneyTransaction.SendByImg = userConnected.Image
 		moneyTransaction.SendToImg = userRecepteur.Image
 		moneyTransaction.SentToName = userRecepteur.AppUserName
 		moneyTransaction.MoneyTransite = append(moneyTransaction.MoneyTransite, body.Value)
 		result := h.DB.Create(moneyTransaction)
 		if result.Error != nil {
-			return nil, fmt.Errorf("creationraw %v", result.Error)
+			return nil, fmt.Errorf("creation raw %v", result.Error)
 		}
 		return &moneyTransaction, nil
 	}
 	fmt.Println("transaction already exist")
+	moneyTransaction.Resume = resume
 	moneyTransaction.MoneyTransite = append(moneyTransaction.MoneyTransite, body.Value)
 	moneyTransaction.Totals = getTotals(moneyTransaction.MoneyTransite)
 	h.DB.Save(&moneyTransaction)
@@ -119,7 +118,7 @@ func (h handler) GetUserByuuid(userReq string) (*models.User, error) {
 	var users models.User
 	res := h.DB.Where("uuid = ? OR app_user_name = ?", userReq, userReq).First(&users)
 	if res.Error != nil {
-		return nil, fmt.Errorf(" %v pas dans uuid et AppUserName", userReq)
+		return nil, fmt.Errorf(" %v: uuid or AppUserName notFound", userReq)
 	}
 	return &users, nil
 }
