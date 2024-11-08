5e019c630ec3aed7e114fe106516f698cdf37741
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-08 18:35:19 +0300

    removing the economies case

--- pkg/Epargne/auto_epargne.go
+++ pkg/Epargne/auto_epargne.go
@@ -13,10 +13,9 @@ import (
 func AutoEpargne(h Handler) error {
 	var epargnes []models.Epargne
 	fmt.Println("cronjob executed", time.Now().Format(time.DateTime))
-	getEpargne := h.DB.Find(&epargnes)
-	if getEpargne.Error != nil {
+	// get all epargne from the db
+	if getEpargne := h.DB.Find(&epargnes); getEpargne.Error != nil {
 		return fmt.Errorf("err on get all epargnes: %v", getEpargne.Error)
-
 	}
 	for _, epargne := range epargnes {
 		user, err := middleware.User.User(middleware.User{UuidToFind: epargne.OwnerUUID, Db: h.DB})
@@ -24,29 +23,19 @@ func AutoEpargne(h Handler) error {
 			return err
 		}
 		if time.Now().Day() == int(epargne.DayPerMounth) {
-			if epargne.Type != "economies" {
-				if err := economiesCase(h, user, &epargne); err != nil {
-					return err
-				}
-				return err
+			user.Moneys = (user.Moneys - epargne.Value)
+			if res := h.DB.Save(&user); res.Error != nil {
+				return errors.New("Error when updating user money" + res.Error.Error())
+			}
+			if createRes := h.DB.Create(&models.EpargneResume{
+				ID:            uuid.New(),
+				Type:          epargne.Type,
+				ResumeMessage: fmt.Sprintf("epargne just got created : value %v , day %v , sent_to %s , owner %s", epargne.Value, epargne.DayPerMounth, epargne.Sent_to, epargne.OwnerUUID),
+				Created_at:    time.Now(),
+			}); createRes.Error != nil {
+				return errors.New("Error occuring creating the epargne resume " + createRes.Error.Error())
 			}
 		}
-	}
-	return nil
-}
-
-func economiesCase(h Handler, user *models.User, epargne *models.Epargne) error {
-	user.Moneys = (user.Moneys - epargne.Value)
-	if res := h.DB.Save(&user); res.Error != nil {
-		return errors.New("Error when updating user money" + res.Error.Error())
-	}
-	if createRes := h.DB.Create(&models.EpargneResume{
-		ID:            uuid.New(),
-		Type:          epargne.Type,
-		ResumeMessage: fmt.Sprintf("epargne just got created : value %v , day %v , sent_to %s , owner %s", epargne.Value, epargne.DayPerMounth, epargne.Sent_to, epargne.OwnerUUID),
-		Created_at:    time.Now(),
-	}); createRes.Error != nil {
-		return errors.New("Error occuring creating the epargne resume " + createRes.Error.Error())
 	}
 	return nil
 }
