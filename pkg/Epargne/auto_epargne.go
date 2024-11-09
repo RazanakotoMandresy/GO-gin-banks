bca42ce77a3132eb3474a426cb749257d403a5e5
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-09 16:43:37 +0300

    documenting the auto epargne , removing economie case

--- pkg/Epargne/auto_epargne.go
+++ pkg/Epargne/auto_epargne.go
@@ -3,6 +3,7 @@ package epargne
 import (
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/RazanakotoMandresy/go-gin-banks/pkg/common/models"
@@ -18,6 +19,7 @@ func AutoEpargne(h Handler) error {
 	if getEpargne := h.DB.Find(&epargnes); getEpargne.Error != nil {
 		return fmt.Errorf("err on get all epargnes: %v", getEpargne.Error)
 	}
+	// get all the epargnes and sort them
 	for _, epargne := range epargnes {
 		user, err := middleware.User.User(middleware.User{UuidToFind: epargne.OwnerUUID, Db: h.DB})
 		if err != nil {
@@ -25,35 +27,38 @@ func AutoEpargne(h Handler) error {
 		}
 		// check day today and if the epargne is autosend mbola todo ny index ny logic false autosend
 		if time.Now().Day() == int(epargne.DayPerMounth) && epargne.AutoSend {
-			if epargne.Type == "economies" || epargne.Type == "economie" {
-				// handle logic economies ,dayPer month just day for the money to be soustract in the current user
-				// the money will be send to himself but only if he click on the user on get my epargne and get the epargne with the specific uuid
-				return nil
-			} else {
-				user.Moneys = (user.Moneys - epargne.Value)
-				if res := h.DB.Save(&user); res.Error != nil {
-					return errors.New("Error when updating user money" + res.Error.Error())
-				}
-				if createRes := h.DB.Create(&models.EpargneResume{
-					ID:            uuid.New(),
-					Type:          epargne.Type,
-					ResumeMessage: fmt.Sprintf("value %v , day %v , sent_to %s , owner %s", epargne.Value, epargne.DayPerMounth, epargne.Sent_to, epargne.OwnerUUID),
-					Created_at:    time.Now(),
-					Value:         uint(epargne.Value),
-				}); createRes.Error != nil {
-					return errors.New("Error occuring creating the epargne resume " + createRes.Error.Error())
-				}
-				userToSend, err := middleware.User.User(middleware.User{UuidToFind: epargne.Sent_to})
-				if err != nil {
-					return err
-				}
-
-				userToSend.Moneys = user.Moneys + epargne.Value
-				if errSaveSend := h.DB.Save(&userToSend); errSaveSend.Error != nil {
-					return err
-				}
+			// handle logic economies ,dayPer month just day for the money to be soustract in the current user
+			// the money will be send to himself but only if he click on the user on get my epargne and get the epargne with the specific uuid
+			if err := autoEpargneCaseNoEconomie(user, &epargne, &h); err != nil {
+				log.Default().Printf("error on epargne ")
 			}
 		}
+		// case non auto send
+		// non auto Send literaly another routes get Epargne
+	}
+	return nil
+}
+func autoEpargneCaseNoEconomie(user *models.User, epargne *models.Epargne, h *Handler) error {
+	user.Moneys = (user.Moneys - epargne.Value)
+	if res := h.DB.Save(&user); res.Error != nil {
+		return errors.New("Error when updating user money" + res.Error.Error())
+	}
+	if createRes := h.DB.Create(&models.EpargneResume{
+		ID:            uuid.New(),
+		Type:          epargne.Type,
+		ResumeMessage: fmt.Sprintf("value %v , day %v , sent_to %s , owner %s", epargne.Value, epargne.DayPerMounth, epargne.Sent_to, epargne.OwnerUUID),
+		Created_at:    time.Now(),
+		Value:         uint(epargne.Value),
+	}); createRes.Error != nil {
+		return errors.New("Error occuring creating the epargne resume " + createRes.Error.Error())
+	}
+	userToSend, err := middleware.User.User(middleware.User{UuidToFind: epargne.Sent_to, Db: h.DB})
+	if err != nil {
+		return err
+	}
+	userToSend.Moneys = user.Moneys + epargne.Value
+	if errSaveSend := h.DB.Save(&userToSend); errSaveSend.Error != nil {
+		return err
 	}
 	return nil
 }
