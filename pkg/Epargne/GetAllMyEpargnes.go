e4a05a87eaed2b2aac603c7ca175f15236dcc408
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-12 12:11:05 +0300

    two getAllEpargnes functions better naming

--- pkg/Epargne/GetAllMyEpargnes.go
+++ pkg/Epargne/GetAllMyEpargnes.go
@@ -19,12 +19,12 @@ func (h Handler) GetAllMyEpargnes(ctx *gin.Context) {
 		return
 	}
 	getEpargnes := getEpargnes{userUUID: userConnected, h: h}
-	myEpargnesEconomies, err := getEpargnes.getAllMyEpargnesFuncEconomies()
+	myEpargnesEconomies, err := getEpargnes.economieCase()
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": err})
 		return
 	}
-	myEpargnesNonEconomie, err := getEpargnes.getAllMyEpargnesFuncNotEconomie()
+	myEpargnesNonEconomie, err := getEpargnes.nonEconomieCase()
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": err})
 		return
@@ -32,7 +32,7 @@ func (h Handler) GetAllMyEpargnes(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"epargnes_economies": myEpargnesEconomies, "simples_epargnes": myEpargnesNonEconomie})
 }
 
-func (g getEpargnes) getAllMyEpargnesFuncEconomies() ([]models.Epargne, error) {
+func (g getEpargnes) economieCase() ([]models.Epargne, error) {
 	var epargnes []models.Epargne
 	res := g.h.DB.Where("owner_uuid = ? AND is_economie = true", g.userUUID).Find(&epargnes)
 	if res.Error != nil {
@@ -40,7 +40,7 @@ func (g getEpargnes) getAllMyEpargnesFuncEconomies() ([]models.Epargne, error) {
 	}
 	return epargnes, nil
 }
-func (g getEpargnes) getAllMyEpargnesFuncNotEconomie() ([]models.Epargne, error) {
+func (g getEpargnes) nonEconomieCase() ([]models.Epargne, error) {
 	var epargnes []models.Epargne
 	res := g.h.DB.Where("owner_uuid = ? AND is_economie = false", g.userUUID).Find(&epargnes)
 	if res.Error != nil {
